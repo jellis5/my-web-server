8a55f7ed7b922cea900a09fb70c4d60264aeea31
Jacob Ellis <jellis5@memphis.edu>
2015-10-27 23:54:48 -0500

    Changed multithreading to pool model

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, threading
+import socket, queue, concurrent.futures
 
 from settings import *
 from client import *
@@ -12,15 +12,27 @@ class Server:
 		self.listen_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.listen_s.bind((HOST, PORT))
 		self.listen_s.listen(QUEUED_CONNS)
-		self.threads = []
+		self.thread_queue = queue.Queue()
+		self.thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+		
+	def serve(self):
+		client_s, client_ip = self.thread_queue.get()
+		req = Request(str(client_s.recv(1024), 'utf-8'))
+		if req.req_method == 'GET':
+			resp = GETResponse(req)
+		elif req.req_method == 'POST':
+			resp = POSTResponse(req)
+		elif req.req_method == 'HEAD':
+			resp = HEADResponse(req)
+		client_s.sendall(bytes(str(resp), 'utf-8'))
+		client_s.close()
 		
 	def run(self):
-		while True:
-			client = Client(*self.listen_s.accept())
-			client.start()
-			self.threads.append(client)
+		with self.thread_pool as executor:
+			while True:
+				self.thread_queue.put(self.listen_s.accept())
+				while not self.thread_queue.empty():
+					executor.submit(self.serve)
 			
 	def close(self):
-		for t in self.threads:
-			t.join()
 		self.listen_s.close()
